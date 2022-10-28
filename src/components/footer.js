2f1208c4dd24ab869b0a30bd3de4bedc92e8cc0c
Aror Emmanuel <emmaaror@gmail.com>
2022-10-28 03:47:53 +0100

    Update to footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,9 @@ import igPic from '../components/ig.svg'
 function Footer() {
     return (
         <div className="footer--box">
-            <img src={twitterPic} className="social--icons" />
-            <img src={whatsappPic} className="social--icons" />
-            <img src={igPic} className="social--icons" />
+            <a href="https://twitter.com/lactoseutd"><img src={twitterPic} className="social--icons" /></a>
+            <a href="https://instagram.com/aj3b0_"><img src={igPic} className="social--icons" /></a>
+            <a href="https://wa.me/2349077471383"><img src={whatsappPic} className="social--icons" /></a>
         </div>
     );
 }
